7e7b24ea9c53fdc40d5584159747274c499ef1ba
aba <bezzatechw1@gmail.com>
2025-09-26 16:39:12 +0700

    Added tests for invalid change personal profile information

--- tests/user/test_profile.py
+++ tests/user/test_profile.py
@@ -59,3 +59,62 @@ def test_change_profile_info(browser, live_server, flush_database):
     submit_button.click()
     # Effect №3
     _ = browser.find_element(By.CSS_SELECTOR, 'div.notification.success')  # INFO! If the test fails, there is an error. 
+
+def test_invalid_change_profile_info(browser, live_server, flush_database):
+    """
+    Test for the "An error occurred while entering data when submitting a personal profile" test case.
+    """
+    username: str = 'user1'
+    password: str = 'o3m4b2!m'
+    last_name: str = 'l'
+    first_name: str = 'f'
+    patronymic: str = 'p'
+    phone: str = '7'
+    about: str = 'a'
+
+    _ = create_test_verify_user(username, password)
+
+    browser.get(live_server.url)
+    login_user(browser, username, password)
+
+    header_link_profile = browser.find_element(By.CSS_SELECTOR, f'a.btn-header[href="/user/profile/{username}/"]')
+    header_link_profile.click()
+    
+    # Cause №1
+    change_profile_link = browser.find_element(By.CSS_SELECTOR, f'a.btn[href="/user/profile/{username}/change/"]')
+    change_profile_link.click()
+    # Effect №1
+    assert browser.current_url.endswith(f'/user/profile/{username}/change/')
+    
+    # Cause №2
+    last_name_input = browser.find_element(By.ID, 'id_last_name')
+    last_name_input.send_keys(last_name)
+
+    first_name_input = browser.find_element(By.ID, 'id_first_name')
+    first_name_input.send_keys(first_name)
+
+    patronymic_input = browser.find_element(By.ID, 'id_patronymic')
+    patronymic_input.send_keys(patronymic)
+
+    display_contacts = browser.find_element(By.ID, 'id_display_contacts')
+    display_contacts.click()
+
+    phone_input = browser.find_element(By.ID, 'id_phone')
+    phone_input.send_keys(phone)
+
+    about_input = browser.find_element(By.ID, 'id_about')
+    about_input.send_keys(about)
+    # Effect №2
+    assert last_name_input.get_attribute('value') == last_name
+    assert first_name_input.get_attribute('value') == first_name
+    assert patronymic_input.get_attribute('value') == patronymic
+    assert display_contacts.is_selected()
+    assert phone_input.get_attribute('value') == phone
+    assert about_input.get_attribute('value') == about
+
+    # Cause №3
+    submit_button = browser.find_element(By.CSS_SELECTOR, 'button.form-button')
+    submit_button.click()
+    # Effect №3
+    _ = browser.find_element(By.CSS_SELECTOR, 'ul.errorlist li')  # INFO! If the test fails, there is an error. 
+    
